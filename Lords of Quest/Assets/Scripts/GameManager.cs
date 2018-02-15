61f00d2d3dfb783e8b4741c930cc353514b16dcf
David O'Sullivan <david.osullivan5@students.ittralee.ie>
2018-02-15 13:39:48 +0000

    Working on Rat Nav Mesh

---
+++ Lords of Quest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private const int NUM_OF_RATS = 5;
+    public int ratsSpawned = 1;
+    public Transform rat;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (ratsSpawned < NUM_OF_RATS)
+            spawnRat();
+    }
+
+    public void spawnRat()
+    {
+        /*GameObject rat = new GameObject();
+        rat.transform.position = randomPosition();
+        rat.AddComponent<RatController>();
+        rat.name = "Rat " + ratsSpawned;
+        ratsSpawned++;*/
+
+        Instantiate(rat, randomPosition(), Quaternion.identity);
+        rat.name = "Rat " + ratsSpawned;
+        ratsSpawned++;
+    }
+
+    public static Vector3 randomPosition()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(-25f, 25f), 1, Random.Range(-25, 25));
+
+        return randomPosition;
+    }
+
+    public  void setRatsSpawned(int ratsSpawned)
+    {
+        this.ratsSpawned = ratsSpawned; 
+    }
+
+    public int getRatsSpawned()
+    {
+        return ratsSpawned;
+    }
+}
--- Lords of Quest/Assets/Scripts/RatController.cs
+++ Lords of Quest/Assets/Scripts/RatController.cs
@@ -1,39 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RatController : MonoBehaviour {
 
     GameObject rat;
-    float currentSpeed = 2f;
-    Collider ratCollider;
-    Rigidbody ratRigidBody;
-    GameObject target;
-    Collider targetCollider;
-
+    //float currentSpeed = 2f;
+    //Collider ratCollider;
+    //Rigidbody ratRigidBody;
+    //GameObject target;
+    //Collider targetCollider;
+    NavMeshAgent navMeshAgent;
+    Transform goal;
+    PlayerController thePlayer;
     // Use this for initialization
     void Start () {
-        rat = gameObject;
-        target = new GameObject();
-        targetCollider = target.AddComponent<SphereCollider>();
-        ratCollider = rat.AddComponent<BoxCollider>();
+        /* rat = gameObject;
+         target = new GameObject();
+         targetCollider = target.AddComponent<SphereCollider>();
+         ratCollider = rat.AddComponent<BoxCollider>();
 
-        target.transform.position = new Vector3(20, 2, 20);
-        rat.transform.position = new Vector3(0, 2, 0);
-	}
+         target.transform.position = new Vector3(20, 2, 20);
+         rat.transform.position = new Vector3(0, 2, 0);*/
+        print("HEL;LO");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        thePlayer = FindObjectOfType<PlayerController>();
+        goal.position = GameManager.randomPosition();
+        rat = gameObject;
+        rat = navMeshAgent.gameObject;
+        navMeshAgent.destination = goal.position;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (rat.transform.position != (target.transform.position - new Vector3(1,0,1)))
+
+        //navMeshAgent.destination = thePlayer.transform.position;
+        float distanceBetweenRatAndTarget = Vector3.Distance(navMeshAgent.transform.position, goal.position);
+        if (distanceBetweenRatAndTarget<.5f)
         {
-            transform.LookAt(target.transform);
-            transform.position += currentSpeed * transform.forward * Time.deltaTime;
+            goal.position = GameManager.randomPosition();
+            navMeshAgent.destination = goal.position;
+            
         }
+            /* if (rat.transform.position != (target.transform.position - new Vector3(1,0,1)))
+         {
+             transform.LookAt(target.transform);
+             transform.position += currentSpeed * transform.forward * Time.deltaTime;
+         }*/
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        target.transform.position = new Vector3(-20, 2, 20);
+        /*if(other == targetCollider)
+            target.transform.position = new Vector3(-20, 2, 20);*/
     }
 }
