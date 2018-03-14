89eea4c9f4afb7f67160cf66e0e3155bcfdb627f
David O'Sullivan <david.osullivan5@students.ittralee.ie>
2018-03-14 14:22:04 +0000

    Did more work on village

--- Lords of Quest/Assets/Scripts/GameManager.cs
+++ Lords of Quest/Assets/Scripts/GameManager.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     private const int NUM_OF_RATS = 4;
+    private const int NUM_OF_HOUSES = 4;
     public static int ratsSpawned = 1;
+    public static int housesSpawned = 1;
+
     public Transform rat;
+    public Transform house;
+    GameObject map;
     
 
     // Use this for initialization
@@ -17,20 +22,35 @@ public class GameManager : MonoBehaviour {
     void Update() {
         if (ratsSpawned <= NUM_OF_RATS)
             spawnRat();
+
+        if (housesSpawned <= NUM_OF_HOUSES)
+            spawnHouse();
     }
 
     public void spawnRat()
     {
-        Transform newRat = Instantiate(rat, randomPosition(), Quaternion.identity);
+        Transform newRat = Instantiate(rat, randomForestPosition(), Quaternion.identity);
         newRat.name = "Rat " + (ratsSpawned + 1);
         ratsSpawned++;
     }
 
-    public static Vector3 randomPosition()
+    public void spawnHouse()
+    {
+        Transform newHouse = Instantiate(house, randomVillagePosition(), Quaternion.identity);
+        newHouse.name = "House" + (housesSpawned + 1);
+        housesSpawned++;
+    }
+
+    public static Vector3 randomForestPosition()
     {
         Vector3 randomPosition = new Vector3(Random.Range(-23f, 23f), 1, Random.Range(-23, 23));
 
         return randomPosition;
+    }
+
+    public static Vector3 randomVillagePosition()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range())
     }
 
     public static int getRatsSpawned()
