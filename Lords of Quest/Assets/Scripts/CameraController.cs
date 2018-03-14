fa63f789b7f30fb933262dfcad96af5f39f6d476
David O'Sullivan <david.osullivan5@students.ittralee.ie>
2018-03-14 13:03:46 +0000

    Rotation FINISHED!!! Began work on village

--- Lords of Quest/Assets/Scripts/CameraController.cs
+++ Lords of Quest/Assets/Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,9 @@ public class CameraController : MonoBehaviour {
     GameObject camera;
     public PlayerController player;
     public Transform playerPosition;
+    private const float TURN_SPEED = 20f;
+
+    private float CameraYLock = 0.3f; // cos of 45 degrees = 0.7071
 
 	// Use this for initialization
 	void Start ()
@@ -20,9 +24,36 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        float rotate = Input.GetAxis("R_YAxis_1");
 
-        if(rotate <= -.05 || rotate >= .05)
-            camera.transform.Rotate(playerPosition.position, rotate);
-	}
+        /*camera.transform.forward = playerPosition.position - new Vector3(-2,1,0);*/
+        float rotate = Input.GetAxis("R_YAxis_1");
+        
+            if (beenAskedToRotateUp(rotate)  && canRotateUp())
+                camera.transform.Rotate(Vector3.left, Space.Self);
+            //camera.transform.Rotate(Vector3.right.z, (TURN_SPEED * Time.deltaTime), rotate, Space.Self);
+
+            else if (beenAskedToRotateDown(rotate) && canRotateDown())
+                transform.Rotate(-Vector3.left, Space.Self);
+
+    }
+
+    private bool canRotateUp()
+    {
+         return transform.forward.y < CameraYLock;
+    }
+
+    private static bool beenAskedToRotateUp(float rotate)
+    {
+        return rotate <= -.05;
+    }
+
+    private bool canRotateDown()
+    {
+        return transform.forward.y > -CameraYLock;
+    }
+
+    private static bool beenAskedToRotateDown(float rotate)
+    {
+        return rotate >= .05;
+    }
 }
